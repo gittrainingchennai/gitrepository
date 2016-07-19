8d33c5c060a6b0e64ae82ddc60a4b0fbc4153a1c
Zeharunisa Begum <zeharunisa.begum@hpe.com>
2016-07-19 03:05:12 -0600

    my second comment

--- gittest/helloworld.java
+++ gittest/helloworld.java
@@ -4,7 +4,8 @@ public class HelloWorld {
          Prints "Hello, World" to the terminal window.
                 System.out.println("Hello, World");
                 System.out.println("first comment");
-                     }
+               System.out.println ("seocnd comment");                
+     }
         
                      }
         
